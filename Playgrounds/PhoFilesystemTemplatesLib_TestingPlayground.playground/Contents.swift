ea26377ec52be6b075bafb831ab61ae8b4fa2df8
CommanderPho <PhoHale@gmail.com>
2021-01-26 16:00:28 -0500

    Successfully wrote and used a template!

--- Playgrounds/PhoFilesystemTemplatesLib_TestingPlayground.playground/Contents.swift
+++ Playgrounds/PhoFilesystemTemplatesLib_TestingPlayground.playground/Contents.swift
@@ -4,50 +4,61 @@ import PhoFilesystemTemplatesLib
 
 func example1() -> FilesystemTemplate {
 	// Example of an NSCS 344 Assignment Folder
-	let Handouts = FilesystemNode(name: "Handouts", children: [])
+	let Handouts = FilesystemNode("Handouts", children: [])
 
 
-	let figureEPSFolder = FilesystemNode(name: "eps", children: [])
-	let figureFolder = FilesystemNode(name: "Figures", children: [figureEPSFolder])
-	let codeHelpersFolder = FilesystemNode(name: "Helpers", children: [])
-	let codeFolder = FilesystemNode(name: "Code", children: [codeHelpersFolder, figureFolder])
+	let figureEPSFolder = FilesystemNode("eps", children: [])
+	let figureFolder = FilesystemNode("Figures", children: [figureEPSFolder])
+	let codeHelpersFolder = FilesystemNode("Helpers", children: [])
+	let codeFolder = FilesystemNode("Code", children: [codeHelpersFolder, figureFolder])
 
-	let submissionFolder = FilesystemNode(name: "Submission", children: [figureFolder, codeFolder])
-
-
-	let photexFigureSymlink = SymlinkNode(name: "Figures", target: figureFolder)
-	let photexFolder = FilesystemNode(name: "photex", children: [photexFigureSymlink])
+	let submissionFolder = FilesystemNode("Submission", children: [figureFolder, codeFolder])
 
 
-	let exampleRootNode = FilesystemNode(name: "Week_07", children: [Handouts, photexFolder, submissionFolder])
+	let photexFigureSymlink = SymlinkNode("Figures", target: figureFolder)
+	let photexFolder = FilesystemNode("photex", children: [photexFigureSymlink])
+
+
+	let exampleRootNode = FilesystemNode("Week_07", children: [Handouts, photexFolder, submissionFolder])
 
 	//New Template Example
 	//let examplePath = URL(fileURLWithPath: "/Users/pho/Dropbox/Classes/NSCS 344/Assignments/")
 	let examplePath = URL(fileURLWithPath: "/Users/pho/Desktop/Classes/NSCS 344/Assignments/")
 
 
-	let newTemplate = FilesystemTemplate(rootNode: exampleRootNode, defaultPath: examplePath)
-
-
-	return newTemplate
-}
-
-
-func xcodeFolderHierarchyTemplate() -> FilesystemTemplate {
-
-	let exampleRootNode = FilesystemNode(name: "root", children: [])
-	let newTemplate = FilesystemTemplate(rootNode: exampleRootNode, defaultPath: examplePath)
+	let newTemplate = FilesystemTemplate(rootNode: exampleRootNode, defaultDeployPath: examplePath)
 
 	return newTemplate
 }
 
 
-let template = example1()
+//func xcodeFolderHierarchyTemplate() -> FilesystemTemplate {
+//
+//	let exampleRootNode = FilesystemNode("root", children: [])
+//	let newTemplate = FilesystemTemplate(rootNode: exampleRootNode, defaultPath: examplePath)
+//
+//	return newTemplate
+//}
 
-template
 
-//let extantContents = try FileManager.default.contentsOfDirectory(at: examplePath, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
-//newTemplate.deploy()
+func deployNsciFolders() {
+	let outFolder = URL(fileURLWithPath: "/Users/pho/Dropbox/Classes/Spring 2020", isDirectory: true)
 
-template.validate()
+	ExampleTemplates.deployUMichClassFolder(withName: "NSCI 700 - Seminar", to: outFolder)
+	ExampleTemplates.deployUMichClassFolder(withName: "NSCI 614 - Sensory Systems NSCI", to: outFolder)
+}
+
+//
+//let template = example1()
+//
+//template
+//
+////let extantContents = try FileManager.default.contentsOfDirectory(at: examplePath, includingPropertiesForKeys: nil, options: .skipsHiddenFiles)
+////newTemplate.deploy()
+//
+//try template.validate()
+
+
+deployNsciFolders()
+
 
---
+++ Sources/PhoFilesystemTemplatesLib/ExampleTemplates/ExampleTemplates.swift
@@ -0,0 +1,59 @@
+//
+//  ExampleTemplates.swift
+//  
+//
+//  Created by Pho Hale on 1/26/21.
+//
+
+import Foundation
+
+public struct ExampleTemplates {
+
+
+
+	public static var xCodeHierarchyTemplate: FilesystemTemplate {
+		let root = FilesystemNode("root", children: [
+			FilesystemNode("Model"),
+			FilesystemNode("Protocols"),
+			FilesystemNode("UI", children: [
+				FilesystemNode("Storyboards"),
+				FilesystemNode("ViewControllers"),
+				FilesystemNode("Views"),
+				FilesystemNode("WindowControllers")
+			])
+		])
+
+		return FilesystemTemplate(rootNode: root, templateName: "XCode Hierarchy Template", templateDescription: "Creates the default hierarchy for my custom packages in XCode.")
+	}
+
+
+	public static var umichSemesterClassFolder: FilesystemTemplate {
+
+		let rootFolderNames = ["Exams", "Assignments", "Notes", "Resources and Handouts"]
+
+		let root = FilesystemNode("root", children: [
+			FilesystemNode("Exams"),
+			FilesystemNode("Assignments"),
+			FilesystemNode("Resources and Handouts"),
+			FilesystemNode("Notes", children: [
+				FilesystemNode("Handwritten Notes")
+			])
+		])
+
+		return FilesystemTemplate(rootNode: root, templateName: "UMich Graduate Class Hierarchy Template", templateDescription: "Creates the default hierarchy used for a new class.")
+	}
+
+
+	public static func deployUMichClassFolder(withName name: String, to: URL) {
+		let template = Self.umichSemesterClassFolder
+		template.rootNode.name = name
+
+		do {
+			try template.deploy(specifyingDeployPath: to)
+			print("Template written to \(to)")
+		} catch let error {
+			fatalError("Failed to create \(name) template at \(to)! Error: \(error)")
+		}
+	}
+
+}
